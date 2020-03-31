81ea6bfff75b22ddb6fa4ceeca6a9747a0dbc716
yeonsang an <ysan1991@gmail.com>
2020-04-01 00:06:02 +0900

    animaleseko 0.1

--- animaleseko/animalese.js
+++ animaleseko/animalese.js
@@ -12,6 +12,48 @@ var Animalese = function(letters_file, onload) {
     }
 
     var processed_script = script;
+    var temp = Hangul.d(script);
+    
+    temp.forEach(function(item, index, arr2){
+        if(item == "ㅏ") temp[index] = "a";
+        if(item == "ㅓ") temp[index] = "eo";
+        if(item == "ㅗ") temp[index] = "o";
+        if(item == "ㅜ") temp[index] = "u";
+        if(item == "ㅡ") temp[index] = "u";
+        if(item == "ㅣ") temp[index] = "i";
+        if(item == "ㅐ") temp[index] = "ae";
+        if(item == "ㅔ") temp[index] = "e";
+        if(item == "ㅑ") temp[index] = "ya";
+        if(item == "ㅕ") temp[index] = "yeo";
+        if(item == "ㅛ") temp[index] = "yo";
+        if(item == "ㅠ") temp[index] = "yu";
+        if(item == "ㅒ") temp[index] = "yae";
+        if(item == "ㅖ") temp[index] = "ye";
+        
+        if(item == "ㄱ") temp[index] = "g";
+        if(item == "ㄲ") temp[index] = "kk";
+        if(item == "ㅋ") temp[index] = "k";
+        if(item == "ㄷ") temp[index] = "d";
+        if(item == "ㄸ") temp[index] = "tt";
+        if(item == "ㅌ") temp[index] = "t";
+        if(item == "ㅂ") temp[index] = "b";
+        if(item == "ㅃ") temp[index] = "pp";
+        if(item == "ㅍ") temp[index] = "p";
+        if(item == "ㅈ") temp[index] = "j";
+        if(item == "ㅉ") temp[index] = "jj";
+        if(item == "ㅊ") temp[index] = "ch";
+        if(item == "ㅅ") temp[index] = "s";
+        if(item == "ㅆ") temp[index] = "ss";
+        if(item == "ㅎ") temp[index] = "h";
+        if(item == "ㄴ") temp[index] = "n";
+        if(item == "ㅁ") temp[index] = "m";
+        if(item == "ㅇ") temp[index] = "";
+        if(item == "ㄹ") temp[index] = "r";
+    });
+
+    processed_script = temp.join('')
+    console.log(processed_script)
+
     if (shorten) {
       processed_script =
         script.replace(/[^a-z]/gi, ' ').split(' ').map(firstLast).join('');
